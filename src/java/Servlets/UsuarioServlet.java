8f215108fac05981da49bd39ab9ae8c70b9f4ffc
João Paulo Dias <jpdias1997@hotmail.com>
2018-04-13 21:49:06 -0300

    Adição Usuario Servlet

---
+++ src/java/Servlets/UsuarioServlet.java
@@ -0,0 +1,38 @@
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "UsuarioServlet", urlPatterns = {"/usuario-listar.html","/usuario-novo.html"})
+public class UsuarioServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if ("/usuario-listar.html".equals(request.getServletPath())) {
+            listarUsuario(response, request);
+        } else if ("/usuario-novo.html".equals(request.getServletPath())) {
+            criarFormUsuario(request, response);
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    private void criarFormUsuario(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void listarUsuario(HttpServletResponse response, HttpServletRequest request) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
