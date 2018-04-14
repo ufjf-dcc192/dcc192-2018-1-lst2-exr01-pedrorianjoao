3d17df95d426e2aa91146cfe393ebdf2138cee5a
Rian Alves <Rian Alves@172.18.132.117>
2018-04-13 21:51:57 -0300

    Altarações na classe usuário

--- src/java/Modelos/Usuario.java
+++ src/java/Modelos/Usuario.java
@@ -1,4 +1,3 @@
-
 package Modelos;
 
 import java.util.ArrayList;
@@ -10,18 +9,39 @@ import java.util.Map;
  * @author Rian das Dores Alves 201676040
  */
 public class Usuario {
-    
-    int idUsuario; 
-    String nomeUsuario;
-    ArrayList<Map<Integer, Figurinha>> mapaQtdFig = new ArrayList<>();
+
+    private int idUsuario;
+    private String nomeUsuario;
+    private ArrayList<Map<Integer, Figurinha>> mapaQtdFig = new ArrayList<>();
 
     public Usuario(int idUsuario, String nomeUsuario, ArrayList<Figurinha> lstFigurinhas) {
         this.idUsuario = idUsuario;
         this.nomeUsuario = nomeUsuario;
-           
+
     }
-    
-    
-    
+
     //mapaNomes.put(1, "João Delfino");
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public ArrayList<Map<Integer, Figurinha>> getMapaQtdFig() {
+        return mapaQtdFig;
+    }
+
+    public void setMapaQtdFig(ArrayList<Map<Integer, Figurinha>> mapaQtdFig) {
+        this.mapaQtdFig = mapaQtdFig;
+    }
 }
